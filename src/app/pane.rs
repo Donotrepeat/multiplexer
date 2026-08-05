4272283d56a34a77ac3b67776b5ad13b494f6000
wouter wijkhuizen <wouterwijkhuizen5@gmail.com>
2026-08-05 20:15:18 +0200

    pane size is corrected

--- src/app/pane.rs
+++ src/app/pane.rs
@@ -91,8 +91,8 @@ impl Pane {
         let parser = self.vpty.lock().unwrap();
         let screen = parser.screen();
         let row = screen.cursor_position().0 as usize;
-        let a = screen.size().0 as usize - 6 - 1 * num_panes;
-        self.scroll_offset = row.saturating_sub(a.saturating_div(5 * num_panes));
+        let a = screen.size().0 as usize - 4;
+        self.scroll_offset = row.saturating_sub(a);
     }
     // Scroll to top (offset = 0)
     pub fn scroll_to_top(&mut self) {
--- src/main.rs
+++ src/main.rs
@@ -9,13 +9,13 @@ use app::pane::Pane;
 fn main() -> Result<()> {
     enable_raw_mode()?;
 
-    let (term_rows, term_cols) = size()?;
+    let (term_cols, term_rows) = size()?;
     println!("the base terminal is of size {term_rows}, {term_cols}");
     let term_rows = term_rows.max(1);
     let term_cols = term_cols.max(1);
 
     let mut app = App {
-        panes: vec![Pane::new(term_rows, term_cols * 4).unwrap()],
+        panes: vec![Pane::new(term_rows, term_cols - 4).unwrap()],
         running: true,
         active: 0,
     };
