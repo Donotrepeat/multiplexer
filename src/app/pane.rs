61b867a1a76345a858766992856d93762afe8d25
wouter wijkhuizen <wouterwijkhuizen5@gmail.com>
2026-08-08 15:59:59 +0200

    added dsr callback

--- src/app/pane.rs
+++ src/app/pane.rs
@@ -6,8 +6,51 @@ use std::usize;
 use anyhow::Result;
 use portable_pty::{native_pty_system, CommandBuilder, MasterPty, PtySize};
 
+pub struct MuxCallbacks {
+    writer: Arc<Mutex<Option<Box<dyn Write + Send>>>>,
+}
+
+impl vt100::Callbacks for MuxCallbacks {
+    fn unhandled_csi(
+        &mut self,
+        screen: &mut vt100::Screen,
+        i1: Option<u8>,
+        _i2: Option<u8>,
+        params: &[&[u16]],
+        c: char,
+    ) {
+        let first = params.first().and_then(|p| p.first()).copied();
+        let reply: Option<Vec<u8>> = match (i1, c) {
+            (None, 'c') => Some(b"\x1b[?62;22c".to_vec()),
+            (Some(b'>'), 'c') => Some(b"\x1b[>0;1;0c".to_vec()),
+            (None, 'n') => match first {
+                Some(5) => Some(b"\x1b[0n".to_vec()),
+                Some(6) => {
+                    let (row, col) = screen.cursor_position();
+                    Some(format!("\x1b[{};{}R", row + 1, col + 1).into_bytes())
+                }
+                _ => None,
+            },
+            (Some(b'?'), 'n') => {
+                if first == Some(6) {
+                    let (row, col) = screen.cursor_position();
+                    Some(format!("\x1b[{};{}R", row + 1, col + 1).into_bytes())
+                } else {
+                    None
+                }
+            }
+            _ => None,
+        };
+        if let Some(bytes) = reply {
+            if let Some(w) = self.writer.lock().unwrap().as_mut() {
+                let _ = w.write_all(&bytes);
+            }
+        }
+    }
+}
+
 pub struct Pane {
-    pub vpty: Arc<Mutex<vt100::Parser>>,
+    pub vpty: Arc<Mutex<vt100::Parser<MuxCallbacks>>>,
     pub pty_writer: Arc<Mutex<Option<Box<dyn Write + Send>>>>,
     pub pty_master: Box<dyn MasterPty>,
     pub screen_changed: Arc<AtomicBool>,
@@ -31,7 +74,14 @@ impl Pane {
         let _child = pair.slave.spawn_command(cmd)?;
         drop(pair.slave);
         let pty_writer = Arc::new(Mutex::new(Some(pair.master.take_writer()?)));
-        let vpty = Arc::new(Mutex::new(vt100::Parser::new(row, coll, 1200)));
+        let vpty = Arc::new(Mutex::new(vt100::Parser::new_with_callbacks(
+            row,
+            coll,
+            1200,
+            MuxCallbacks {
+                writer: Arc::clone(&pty_writer),
+            },
+        )));
         let vpt_clone = Arc::clone(&vpty);
 
         let screen_changed = Arc::new(AtomicBool::new(true));
@@ -119,5 +169,87 @@ impl Pane {
     // Check if at bottom
     pub fn at_bottom(&self) -> bool {
         self.get_scroll_offset() >= 1200
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    struct TestWriter(Arc<Mutex<Vec<u8>>>);
+
+    impl Write for TestWriter {
+        fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
+            self.0.lock().unwrap().extend_from_slice(buf);
+            Ok(buf.len())
+        }
+
+        fn flush(&mut self) -> std::io::Result<()> {
+            Ok(())
+        }
+    }
+
+    fn test_parser() -> (vt100::Parser<MuxCallbacks>, Arc<Mutex<Vec<u8>>>) {
+        let bytes = Arc::new(Mutex::new(Vec::new()));
+        let writer: Box<dyn Write + Send> = Box::new(TestWriter(Arc::clone(&bytes)));
+        let writer = Arc::new(Mutex::new(Some(writer)));
+        let parser = vt100::Parser::new_with_callbacks(
+            24,
+            80,
+            1200,
+            MuxCallbacks {
+                writer: Arc::clone(&writer),
+            },
+        );
+        (parser, bytes)
+    }
+
+    #[test]
+    fn da1_replies_vt220() {
+        let (mut parser, bytes) = test_parser();
+        parser.process(b"\x1b[c");
+        assert_eq!(*bytes.lock().unwrap(), b"\x1b[?62;22c".to_vec());
+    }
+
+    #[test]
+    fn dsr5_replies_ok() {
+        let (mut parser, bytes) = test_parser();
+        parser.process(b"\x1b[5n");
+        assert_eq!(*bytes.lock().unwrap(), b"\x1b[0n".to_vec());
+    }
+
+    #[test]
+    fn cpr_reports_one_based_position() {
+        let (mut parser, bytes) = test_parser();
+        parser.process(b"\x1b[3;5H\x1b[6n");
+        assert_eq!(*bytes.lock().unwrap(), b"\x1b[3;5R".to_vec());
+    }
+
+    #[test]
+    fn cpr_reports_home_as_1_1() {
+        let (mut parser, bytes) = test_parser();
+        parser.process(b"\x1b[H\x1b[6n");
+        assert_eq!(*bytes.lock().unwrap(), b"\x1b[1;1R".to_vec());
+    }
+
+    #[test]
+    fn private_cpr_is_answered() {
+        let (mut parser, bytes) = test_parser();
+        parser.process(b"\x1b[2;2H\x1b[?6n");
+        assert_eq!(*bytes.lock().unwrap(), b"\x1b[2;2R".to_vec());
+    }
+
+    #[test]
+    fn da2_replies_generic_terminal() {
+        let (mut parser, bytes) = test_parser();
+        parser.process(b"\x1b[>0c");
+        assert_eq!(*bytes.lock().unwrap(), b"\x1b[>0;1;0c".to_vec());
+    }
+
+    #[test]
+    fn printer_status_is_ignored() {
+        let (mut parser, bytes) = test_parser();
+        parser.process(b"\x1b[?5n");
+        assert!(bytes.lock().unwrap().is_empty());
     }
 }
